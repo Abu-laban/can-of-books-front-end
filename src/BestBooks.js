3405eb3328c941f5de87ed3a4fed03081b321f6e
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-07-13 18:14:47 +0300

    finish lab13

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -5,6 +5,8 @@ import './BestBooks.css';
 import axios from 'axios';
 import { withAuth0 } from '@auth0/auth0-react';
 import MyBooks from './components/MyBooks';
+import BookFormModal from './components/BookFormModal';
+import Button from 'react-bootstrap/Button'
 
 class MyFavoriteBooks extends React.Component {
 
@@ -12,7 +14,8 @@ class MyFavoriteBooks extends React.Component {
     super(props);
     this.state = {
       books: [],
-      showModal: false
+      showCards: false,
+      showModal: false,
     }
   }
 
@@ -25,7 +28,7 @@ class MyFavoriteBooks extends React.Component {
 
       this.setState({
         books: bookReader.data,
-        showModal: true
+        showCards: true
       });
 
     } catch (error) {
@@ -33,16 +36,78 @@ class MyFavoriteBooks extends React.Component {
     }
   }
 
+  handleModalShowing = () => {
+    this.setState({ showModal: true })
+
+  }
+
+  handleModalClosing = () => {
+    this.setState({ showModal: false })
+  }
+
+  addBook = async (event) => {
+    event.preventDefault();
+
+    const bookFormData = {
+      email: this.props.auth0.user.email,
+      bookName: event.target.bName.value,
+      bookDescription: event.target.bDescription.value,
+      bookStatus: event.target.bStatus.value,
+      bookImg: event.target.bImg.value,
+    }
+    try {
+      const SERVER = process.env.REACT_APP_SERVER_URL;
+
+      const booksData = await axios.post(`${SERVER}/books`, bookFormData)
+
+      this.setState({
+        books: booksData.data
+      })
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  deleteBook = async (id) => {
+
+
+
+    try {
+      const SERVER = process.env.REACT_APP_SERVER_URL;
+
+      const booksData = await axios.delete(`${SERVER}/books/${id}?email=${this.props.auth0.user.email}`)
+
+      this.setState({
+        books: booksData.data
+      })
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+
   render() {
     return (
       <Jumbotron>
+
         <h1>My Favorite Books</h1>
+
         <p>
           This is a collection of my favorite books
         </p>
+
+        <Button variant="outline-dark" onClick={this.handleModalShowing}>Add Book</Button>
+
         <MyBooks
           books={this.state.books}
-          showModal={this.state.showModal} />
+          showCards={this.state.showCards}
+          deleteBook={this.deleteBook} />
+
+        <>
+          <BookFormModal addBook={this.addBook} showModal={this.state.showModal} closing={this.handleModalClosing} />
+        </>
       </Jumbotron>
     )
   }
